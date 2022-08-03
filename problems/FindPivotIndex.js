0b875872e1aae1b35332c9f25e990c2870e8b8e8
amandahp <amandahoffmann96@gmail.com>
2022-08-02 21:44:01 -0300

    feat: add fast solution

---
+++ problems/FindPivotIndex.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+ function pivotIndex(nums){
+  if(nums.length === 0) return -1;
+  if(nums.length === 1) return 0;
+
+    const totalSum = nums.reduce((acc, cur) => acc + cur);
+    let leftSum = 0;
+
+    for(i = 0; i < nums.length; i++) {
+        if(leftSum === totalSum - leftSum - nums[i]) {
+            return i;
+        }
+        leftSum += nums[i];
+    };
+
+    return -1;
+
+};
